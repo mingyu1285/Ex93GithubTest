59214a702a14602cdf9eb2491c86f3dca0f50e85
mingyu1285 <56614072+mingyu1285@users.noreply.github.com>
2020-07-03 14:40:19 +0900

    Update MainActivity.java

--- app/src/main/java/com/minkyu1285/ex93githubtest/MainActivity.java
+++ app/src/main/java/com/minkyu1285/ex93githubtest/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int a =10;
+        int a = 10;
+        int b = 20;
     }
 }
