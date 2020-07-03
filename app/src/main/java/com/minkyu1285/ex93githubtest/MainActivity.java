5db704a28f5a035d8021bd94f93cd7992666f4b8
Minkyu <mingyu1285@naver.com>
2020-07-03 14:34:18 +0900

    Initial commit

---
+++ app/src/main/java/com/minkyu1285/ex93githubtest/MainActivity.java
@@ -0,0 +1,14 @@
+package com.minkyu1285.ex93githubtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
